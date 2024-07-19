2e4f47f869249869199e06f93383ae3db71f9797
vkflco08 <vkflco08@naver.com>
2024-07-19 16:19:53 +0900

    fix cors error

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8181;
+const corsOrigin = process.env.CORS_ORIGIN || 'http://localhost';
+
+// CORS 설정
+app.use(cors({
+  origin: corsOrigin,
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204
+}));
 
 app.get('/', (req, res) => {
   res.send('Hello, Express!, end point /');
