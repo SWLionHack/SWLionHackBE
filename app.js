6084107115d25a15912c30bf4f19f92f095636f9
vkflco08 <vkflco08@naver.com>
2024-07-19 16:02:39 +0900

    react connect test #1

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ const app = express();
 const port = process.env.PORT || 8181;
 
 app.get('/', (req, res) => {
-  res.send('Hello, Express!');
+  res.send('Hello, Express!, end point /');
 });
 
+app.get('/api', (req, res) => {
+  res.send('Hello, Express!, end point /api');
+});
+
+
 app.listen(port, () => {
-  console.log(`Server running on http://soulmate.pe.kr:${port}`);
+  console.log(`Server running on :${port}`);
 });
