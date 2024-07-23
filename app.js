f08e813928ea46767a30ef31a9eb8a1ac3f1fd88
오정빈 <126961816+LeoOH5@users.noreply.github.com>
2024-07-23 09:09:13 +0900

    JTW

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+require('dotenv').config();
 const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
 const cors = require('cors');
-const dotenv = require('dotenv');
-
-// 환경 변수 로드
-dotenv.config();
+const bcrypt = require('bcrypt');
+const { router, authenticateJWT } = require('./routes/router'); // router와 authenticateJWT 가져오기
+const sequelize = require('./sequelize');
+const User = require('./models/User');
+const { getTest } = require('./test/testRepository.js');
 
 const app = express();
 const port = process.env.PORT || 8181;
@@ -17,19 +21,52 @@ app.use(cors({
   optionsSuccessStatus: 204
 }));
 
-const { getTest } = require('./test/testRepository.js');
+const initializeApp = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+    await sequelize.sync({ force: true });  // 데이터베이스 동기화 (force: true로 설정하여 기존 테이블을 삭제하고 다시 생성)
+    console.log('Database synchronized');
 
-app.get('/test', async (req, res) => { // req를 추가했습니다.
+    // 목업 데이터 삽입
+    await User.bulkCreate([
+      { name: 'Alice3', phone: '010-1111-1111', email: '123@gmail.com', password: await bcrypt.hash('123', 10), status: "자녀" },
+      { name: 'Bob3', phone: '010-2222-2222', email: '456@gmail.com', password: await bcrypt.hash('456', 10), status: "부모" },
+      { name: 'Charlie3', phone: '010-3333-3333', email: '789@gmail.com', password: await bcrypt.hash('789', 10), status: "자녀" },
+    ]);
+
+    console.log('Mock data inserted');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+
+initializeApp();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'src', 'views'));
+
+// 라우터 사용
+app.use("/", router);
+
+// 기존 app.js의 엔드포인트 유지
+app.get('/test', async (req, res) => {
   try {
     const data = await getTest();
     res.json({ message: 'Hello, Express!, end point /test', data: data });
   } catch (err) {
-    res.status(500).json({ message: 'Database Error', error: err.message }); // 상태 코드 추가
+    res.status(500).json({ message: 'Database Error', error: err.message });
   }
 });
 
 app.get('/api', (req, res) => {
   res.json({ message: 'Hello, Express!, end point /api' });
+});
+
+// JWT 보호된 엔드포인트 예시
+app.get('/protected', authenticateJWT, (req, res) => {
+  res.status(200).send('This is a protected route');
 });
 
 app.listen(port, () => {
