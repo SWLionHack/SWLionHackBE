97ff4cfc18a84573fa5ed62f3865fe3a7ff1bad4
오정빈 <126961816+LeoOH5@users.noreply.github.com>
2024-07-24 11:50:38 +0900

    DB 변수명 맞추기

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();  // 환경 변수 로드
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.DBHOST,
+  user: process.env.DBUSER,
+  password: process.env.DBPASSWORD,
+  database: process.env.DBNAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
